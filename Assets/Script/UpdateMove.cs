759f2de6d6d864dcdff247e1ae3291cb1f313a8d
Diego Vallina <a45933@alunos.ipb.pt>
2024-10-23 16:46:33 +0100

    Initial commit

---
+++ Assets/Script/UpdateMove.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateMove : MonoBehaviour
+{
+    public float speed = 0.5f;
+
+    void Update()
+    {
+        this.transform.Translate(0, 0, Time.deltaTime * speed);
+    }
+}
